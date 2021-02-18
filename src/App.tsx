a091b367f6c2505fdc9a11231ee1834a02693b93
Boris <rozumniy.boris.work@gmail.com>
2021-02-18 07:06:24 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Container } from './Components/Container';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 
 export const App = () => {
   return (
     <Container>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-        </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/page1">
+            <h1>page1</h1>
+          </Route>
+          <Route path="/page2">
+            <h1>page2</h1>
+          </Route>
+          <Redirect to="/page1" />
+        </Switch>
+      </BrowserRouter>
     </Container>
   );
 }
