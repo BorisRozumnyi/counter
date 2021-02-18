700e8da07e2bb5e41c9599fad05e78a80cc7905f
Boris <rozumniy.boris.work@gmail.com>
2021-02-18 15:56:49 +0200

    create redux directory

---
+++ src/redux/store.ts
@@ -0,0 +1,26 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { counter } from './reducers';
+import { mainSaga } from './sagas/mainSaga';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+    REDUX_INITIAL_DATA: any;
+  }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
+
+const sagaMiddleware = createSagaMiddleware();
+
+export const store = createStore(
+  counter,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(mainSaga);
+
+// const action = type => store.dispatch({type})
